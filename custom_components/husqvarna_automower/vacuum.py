4291432fae381527fe7995a6aa861c9df4c4f303
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2020-09-04 18:25:36 +0200

    Update vacuum.py

--- custom_components/husqvarna_automower/vacuum.py
+++ custom_components/husqvarna_automower/vacuum.py
@@ -64,24 +64,27 @@ class husqvarna_automowerVacuum(HusqvarnaEntity, StateVacuumEntity):
         self.mower_timestamp = (self.mower_attributes['metadata']['statusTimestamp'])/1000
         self.mower_local_timestamp = time.localtime(self.mower_timestamp)
         self.readable_mower_local_timestamp = time.strftime("%Y-%m-%d %H:%M:%S", self.mower_local_timestamp)
-        if self.mower_attributes['mower']['state'] == "IN_OPERATION":
-            return f"{self.mower_attributes['mower']['activity']}"
-        elif self.mower_attributes['mower']['state'] == "FATAL_ERROR":
-            self.error_code = self.mower_attributes['mower']['errorCode']
-            return ERRORCODES.get(self.error_code)
-        elif self.mower_attributes['mower']['state'] == "ERROR":
-            self.error_code = self.mower_attributes['mower']['errorCode']
-            return ERRORCODES.get(self.error_code)
-        elif self.mower_attributes['mower']['state'] == "ERROR_AT_POWER_UP":
-            self.error_code = self.mower_attributes['mower']['errorCode']
-            return ERRORCODES.get(self.error_code)
-        elif self.mower_attributes['mower']['state'] == "RESTRICTED":
-            if self.mower_attributes['planner']['restrictedReason'] == "NOT_APPLICABLE":
-                return "Parked until further notice"
+        if self.mower_attributes['metadata']['connected'] == False:
+            return "Not connected"
+        elif self.mower_attributes['metadata']['connected'] == True:
+            if self.mower_attributes['mower']['state'] == "IN_OPERATION":
+                return f"{self.mower_attributes['mower']['activity']}"
+            elif self.mower_attributes['mower']['state'] == "FATAL_ERROR":
+                self.error_code = self.mower_attributes['mower']['errorCode']
+                return ERRORCODES.get(self.error_code)
+            elif self.mower_attributes['mower']['state'] == "ERROR":
+                self.error_code = self.mower_attributes['mower']['errorCode']
+                return ERRORCODES.get(self.error_code)
+            elif self.mower_attributes['mower']['state'] == "ERROR_AT_POWER_UP":
+                self.error_code = self.mower_attributes['mower']['errorCode']
+                return ERRORCODES.get(self.error_code)
+            elif self.mower_attributes['mower']['state'] == "RESTRICTED":
+                if self.mower_attributes['planner']['restrictedReason'] == "NOT_APPLICABLE":
+                    return "Parked until further notice"
+                else:
+                    return f"{self.mower_attributes['planner']['restrictedReason']}"
             else:
-                return f"{self.mower_attributes['planner']['restrictedReason']}"
-        else:
-            return f"{self.mower_attributes['mower']['state']}"
+                return f"{self.mower_attributes['mower']['state']}"
         # else:
         #     """return the state, if api respone is not 200""""
         #     return self.coordinator.data['api_status_code']
