9bb7046fdaf8d40918296cf0428c3733f18e4280
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2020-10-20 23:30:53 +0200

    Update for 0.5

--- custom_components/husqvarna_automower/vacuum.py
+++ custom_components/husqvarna_automower/vacuum.py
@@ -42,13 +42,13 @@ async def async_setup_entry(hass, entry, async_add_devices):
     coordinator = hass.data[DOMAIN][entry.entry_id]
 
     async_add_devices(
-        husqvarna_automowerVacuum(coordinator, idx) for idx, ent in enumerate(coordinator.data['data'])
+        HusqvarnaAutomowerEntity(coordinator, idx) for idx, ent in enumerate(coordinator.data['data'])
     )
 
 
 class HusqvarnaEntity(entity.Entity):
     """Defining the Husqvarna Entity."""
-    
+
     def __init__(self, coordinator):
         """Pass the coordinator to the class."""
         self.coordinator = coordinator
@@ -64,13 +64,20 @@ class HusqvarnaEntity(entity.Entity):
         await self.coordinator.async_request_refresh()
 
 
-class husqvarna_automowerVacuum(HusqvarnaEntity, StateVacuumEntity, CoordinatorEntity):
+class HusqvarnaAutomowerEntity(HusqvarnaEntity, StateVacuumEntity, CoordinatorEntity):
     """Defining each mower Entity."""
-    
+
     def __init__(self, coordinator, idx):
         """Pass coordinator to CoordinatorEntity."""
         super().__init__(coordinator)
         self.idx = idx
+        self.mower = self.coordinator.data['data'][self.idx]
+        self.mower_attributes = self.mower['attributes']
+        self.access_token = self.coordinator.data['token']['access_token']
+        self.provider = self.coordinator.data['token']['provider']
+        self.token_type = self.coordinator.data['token']['token_type']
+        self.mower_id = self.mower['id']
+        self.api_key = self.coordinator.data['api_key']
 
     @property
     def available(self):
@@ -91,7 +98,6 @@ class husqvarna_automowerVacuum(HusqvarnaEntity, StateVacuumEntity, CoordinatorE
     def state(self):
         """Return the state of the mower."""
         self.mower_attributes = self.coordinator.data['data'][self.idx]['attributes']
-        self.api_key = self.coordinator.data['api_key']
         self.mower_timestamp = (self.mower_attributes['metadata']['statusTimestamp'])/1000
         self.mower_local_timestamp = time.localtime(self.mower_timestamp)
         self.readable_mower_local_timestamp = time.strftime("%Y-%m-%d %H:%M:%S", self.mower_local_timestamp)
@@ -129,21 +135,21 @@ class husqvarna_automowerVacuum(HusqvarnaEntity, StateVacuumEntity, CoordinatorE
     @property
     def battery_level(self):
         """Return the current battery level of the mower."""
-        return max(0, min(100, self.mower_attributes['battery']['batteryPercent']))
+        return max(0, min(100, self.coordinator.data['data'][self.idx]['attributes']['battery']['batteryPercent']))
 
     @property
     def device_state_attributes(self):
         """Return the specific state attributes of this mower."""
         self.mower_attributes = self.coordinator.data['data'][self.idx]['attributes']
-        if self.mower_attributes['mower']['errorCodeTimestamp'] == 0:
+        if self.coordinator.data['data'][self.idx]['attributes']['mower']['errorCodeTimestamp'] == 0:
             self.attr_errorCodeTimestamp = "-"
         else:
-            self.attr_errorCodeTimestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime((self.mower_attributes['mower']['errorCodeTimestamp'])/1000))
+            self.attr_errorCodeTimestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime((self.coordinator.data['data'][self.idx]['attributes']['mower']['errorCodeTimestamp'])/1000))
 
-        if self.mower_attributes['planner']['nextStartTimestamp'] == 0:
+        if self.coordinator.data['data'][self.idx]['attributes']['planner']['nextStartTimestamp'] == 0:
             self.attr_nextStartTimestamp = "-"
         else:
-            self.attr_nextStartTimestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime((self.mower_attributes['planner']['nextStartTimestamp'])/1000))
+            self.attr_nextStartTimestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime((self.coordinator.data['data'][self.idx]['attributes']['planner']['nextStartTimestamp'])/1000))
 
         self.attributes = {
             "mode": self.mower_attributes['mower']['mode'],
@@ -161,48 +167,42 @@ class husqvarna_automowerVacuum(HusqvarnaEntity, StateVacuumEntity, CoordinatorE
         return self.attributes
 
 
-    def pause(self, **kwargs):
-        """Pauses the mower."""
-        self.access_token = self.coordinator.data['token']['access_token']
-        self.provider = self.coordinator.data['token']['provider']
-        self.token_type = self.coordinator.data['token']['token_type']
-        self.mower_id = self.coordinator.data['data'][self.idx]['id']
-        self.mower_command = Return(self.api_key, self.access_token, self.provider, self.token_type, self.mower_id)
-        self.mower_command.mower_pause()
-        self._state = "Pausing"
-
-    def stop(self, **kwargs):
-        """Parks the mower until next schedule."""
-        self.access_token = self.coordinator.data['token']['access_token']
-        self.provider = self.coordinator.data['token']['provider']
-        self.token_type = self.coordinator.data['token']['token_type']
-        self.mower_id = self.coordinator.data['data'][self.idx]['id']
-        self.mower_command = Return(self.api_key, self.access_token, self.provider, self.token_type, self.mower_id)
-        self.mower_command.mower_parkuntilnextschedule()
-        self._state = "WEEK_SCHEDULE"
-
-
-    def return_to_base(self, **kwargs):
-        """Parks the mower until further notice."""
-        self.access_token = self.coordinator.data['token']['access_token']
-        self.provider = self.coordinator.data['token']['provider']
-        self.token_type = self.coordinator.data['token']['token_type']
-        self.mower_id = self.coordinator.data['data'][self.idx]['id']
-        self.mower_command = Return(self.api_key, self.access_token, self.provider, self.token_type, self.mower_id)
-        self.mower_command.mower_parkuntilfurthernotice()
-        self._state = "Parked until further notice"
-
-    def start(self, **kwargs):
+    async def async_start(self, **kwargs):
         """Resume schedule."""
-        self.access_token = self.coordinator.data['token']['access_token']
-        self.provider = self.coordinator.data['token']['provider']
-        self.token_type = self.coordinator.data['token']['token_type']
-        self.mower_id = self.coordinator.data['data'][self.idx]['id']
-        self.mower_command = Return(self.api_key, self.access_token, self.provider, self.token_type, self.mower_id)
-        self.mower_command.mower_resumeschedule()
-        self.schedule_update_ha_state()
-        self._state = "WEEK_SCHEDULE"
+        self.payload = '{"data": {"type": "ResumeSchedule"}}'
+        self.mower_command = Return(self.api_key, self.access_token, self.provider, self.token_type, self.mower_id, self.payload)
+        try:
+            await self.mower_command.async_mower_command()
+            await self.coordinator.async_request_refresh()
+        except Exception as exception:
+            raise UpdateFailed(exception)
 
-    def __set_state_to_dock(self):
-        self._state = STATE_DOCKED
-        self.schedule_update_ha_state()
+    async def async_async_pause(self, **kwargs):
+        """Pauses the mower."""
+        self.payload = '{"data": {"type": "Pause"}}'
+        self.mower_command = Return(self.api_key, self.access_token, self.provider, self.token_type, self.mower_id, self.payload)
+        try:
+            await self.mower_command.async_mower_command()
+            await self.coordinator.async_request_refresh()
+        except Exception as exception:
+            raise UpdateFailed(exception)
+
+    async def async_stop(self, **kwargs):
+        """Parks the mower until next schedule."""
+        self.payload = '{"data": {"type": "ParkUntilNextSchedule"}}'
+        self.mower_command = Return(self.api_key, self.access_token, self.provider, self.token_type, self.mower_id, self.payload)
+        try:
+            await self.mower_command.async_mower_command()
+            await self.coordinator.async_request_refresh()
+        except Exception as exception:
+            raise UpdateFailed(exception)
+
+    async def async_return_to_base(self, **kwargs):
+        """Parks the mower until further notice."""
+        self.payload = '{"data": {"type": "ParkUntilFurtherNotice"}}'
+        self.mower_command = Return(self.api_key, self.access_token, self.provider, self.token_type, self.mower_id, self.payload)
+        try:
+            await self.mower_command.async_mower_command()
+            await self.coordinator.async_request_refresh()
+        except Exception as exception:
+            raise UpdateFailed(exception)
