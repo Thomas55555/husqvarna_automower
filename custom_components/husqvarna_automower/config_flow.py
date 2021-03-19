65788fd63035f3feda18a678584e13617802b530
Thomas5555 <thomas.protzner@gmail.com>
2021-03-20 00:02:23 +0100

    add api-key to config title

--- custom_components/husqvarna_automower/config_flow.py
+++ custom_components/husqvarna_automower/config_flow.py
@@ -75,7 +75,7 @@ class HusqvarnaConfigFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self._abort_if_unique_id_configured()
 
         return self.async_create_entry(
-            title="Husqvarna Automower Integration",
+            title=user_input[CONF_API_KEY],
             data={
                 CONF_USERNAME: user_input[CONF_USERNAME],
                 CONF_PASSWORD: user_input[CONF_PASSWORD],
