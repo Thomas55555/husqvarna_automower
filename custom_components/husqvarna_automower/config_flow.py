f105230f6e333dcb2d18222dd2ed8314f5064ce3
Thomas5555 <thomas.protzner@gmail.com>
2022-04-26 15:32:36 +0000

    add menu

--- custom_components/husqvarna_automower/config_flow.py
+++ custom_components/husqvarna_automower/config_flow.py
@@ -5,7 +5,7 @@ from aiohttp.client_exceptions import ClientConnectorError, ClientResponseError
 import voluptuous as vol
 
 from aioautomower import GetAccessToken, GetMowerData, TokenError
-from homeassistant import config_entries
+from homeassistant import config_entries, data_entry_flow
 from homeassistant.const import (
     CONF_ACCESS_TOKEN,
     CONF_API_KEY,
@@ -19,16 +19,14 @@ from homeassistant.helpers.typing import ConfigType
 
 from .const import CONF_PROVIDER, CONF_TOKEN_TYPE, DOMAIN
 from .oauth_impl import OndiloOauth2Implementation
-
-OAUTH2_CLIENTID = "572bef0c-6e2f-4dd0-b7f6-2c96ea7de3b5"
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_entry_oauth2_flow
-
+OAUTH2_CLIENTID = "client_id"
 _LOGGER = logging.getLogger(__name__)
 
 
 class HusqvarnaConfigFlowHandler(
-    config_entry_oauth2_flow.AbstractOAuth2FlowHandler, domain=DOMAIN
+    config_entry_oauth2_flow.AbstractOAuth2FlowHandler, data_entry_flow.FlowHandler, domain=DOMAIN
 ):
 
     """Handle a config flow."""
@@ -41,19 +39,27 @@ class HusqvarnaConfigFlowHandler(
 
         data_schema = {
             vol.Required(CONF_API_KEY): vol.All(str, vol.Length(min=36, max=36)),
-            vol.Required(OAUTH2_CLIENTID): vol.All(str, vol.Length(min=36, max=36)),
+            vol.Required(CONF_USERNAME): str,
+            vol.Required(CONF_PASSWORD): str,
         }
 
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema(data_schema), errors=errors
+            step_id="password", data_schema=vol.Schema(data_schema), errors=errors
         )
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+        _LOGGER.debug("test")
+        return self.async_show_menu(
+            step_id="user",
+            menu_options=["oauth2", "password"]
+        )
+
+    async def async_step_oauth2(self, user_input=None):
 
         errors = {}
 
-        await self.async_set_unique_id(OAUTH2_CLIENTID)
+        await self.async_set_unique_id(self.hass.data[DOMAIN][OAUTH2_CLIENTID])
 
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
@@ -64,7 +70,109 @@ class HusqvarnaConfigFlowHandler(
 
         return await super().async_step_user(user_input)
 
+    async def async_step_password(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        _LOGGER.debug("user input %s", user_input)
+        if user_input is None:
+            return await self._show_setup_form(errors)
+        _LOGGER.debug("Password2")
+        try:
+            get_token = GetAccessToken(
+                user_input[CONF_API_KEY],
+                user_input[CONF_USERNAME],
+                user_input[CONF_PASSWORD],
+            )
+            access_token_raw = await get_token.async_get_access_token()
+            _LOGGER.debug("Password23")
+        except (ClientConnectorError, TokenError):
+            # On 400 credentials could be wrong, or (Authentication API && Automower Connect API) are not connected.
+            errors["base"] = "auth"
+            return await self._show_setup_form(errors)
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "auth"
+            return await self._show_setup_form(errors)
 
+        await self.async_test_mower(user_input,access_token_raw)
+
+        unique_id = user_input[CONF_API_KEY]
+        data = {
+            CONF_API_KEY: user_input[CONF_API_KEY],
+            CONF_TOKEN: access_token_raw,
+        }
+
+        existing_entry = await self.async_set_unique_id(unique_id)
+
+        if existing_entry:
+            self.hass.config_entries.async_update_entry(existing_entry, data=data)
+            await self.hass.config_entries.async_reload(existing_entry.entry_id)
+            return self.async_abort(reason="reauth_successful")
+
+        return self.async_create_entry(
+            title=user_input[CONF_API_KEY],
+            data=data,
+        )
+
+
+
+    async def async_test_mower(self, user_input, access_token_raw):
+
+        try:
+            get_mower_data = GetMowerData(
+                user_input[CONF_API_KEY],
+                access_token_raw[CONF_ACCESS_TOKEN],
+                access_token_raw[CONF_PROVIDER],
+                access_token_raw[CONF_TOKEN_TYPE],
+            )
+            mower_data = await get_mower_data.async_mower_state()
+            _LOGGER.debug("config: %s", mower_data)
+        except (ClientConnectorError, ClientResponseError):
+            if "amc:api" in access_token_raw["scope"]:
+                errors["base"] = "api_key"  ## Something's wrong with the key
+            else:
+                errors["base"] = "mower"  ## Automower Connect API not connected
+            return await self._show_setup_form(errors)
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+            return await self._show_setup_form(errors)
+
+        if "amc:api" not in access_token_raw["scope"]:
+            # If the API-Key is old
+            errors["base"] = "api_key"
+            return await self._show_setup_form(errors)
+
+
+
+
+    async def async_step_finish (self, user_input=None):
+
+        existing_entry = await self.async_set_unique_id(unique_id)
+
+        if existing_entry:
+            self.hass.config_entries.async_update_entry(existing_entry, data=data)
+            await self.hass.config_entries.async_reload(existing_entry.entry_id)
+            return self.async_abort(reason="reauth_successful")
+
+        return self.async_create_entry(
+            title=user_input[CONF_API_KEY],
+            data=data,
+        )
+
+
+    async def async_step_reauth(self, user_input=None):
+        """Perform reauth upon an API authentication error."""
+        return await self.async_step_reauth_confirm()
+
+    async def async_step_reauth_confirm(self, user_input=None):
+        """Dialog that informs the user that reauth is required."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="reauth_confirm",
+                data_schema=vol.Schema({}),
+            )
+        return await self.async_step_user()
 
     @property
     def logger(self) -> logging.Logger:
