207943b970e91bfc08bb9424d8d0f3938840ccc5
Thomas5555 <thomas.protzner@gmail.com>
2022-04-29 15:44:55 +0000

    optimizations

--- custom_components/husqvarna_automower/__init__.py
+++ custom_components/husqvarna_automower/__init__.py
@@ -6,7 +6,6 @@ import voluptuous as vol
 import aioautomower
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
-    CONF_API_KEY,
     CONF_CLIENT_ID,
     CONF_CLIENT_SECRET,
     CONF_PASSWORD,
@@ -15,13 +14,20 @@ from homeassistant.const import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryAuthFailed
-from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers import config_entry_oauth2_flow, config_validation as cv
 from homeassistant.helpers.typing import ConfigType
 
-from .const import DOMAIN, HUSQVARNA_URL, PLATFORMS, STARTUP_MESSAGE
+from . import config_flow
+from .const import (
+    DOMAIN,
+    HUSQVARNA_URL,
+    OAUTH2_AUTHORIZE,
+    OAUTH2_TOKEN,
+    PLATFORMS,
+    STARTUP_MESSAGE,
+)
 
 _LOGGER = logging.getLogger(__name__)
-
 
 CONFIG_SCHEMA = vol.Schema(
     {
@@ -39,19 +45,35 @@ CONFIG_SCHEMA = vol.Schema(
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up the Google component."""
-    if hass.data.get(DOMAIN) is None:
-        hass.data.setdefault(DOMAIN, {})
-        _LOGGER.info(STARTUP_MESSAGE)
-    conf = config.get(DOMAIN, {})
-    _LOGGER.debug("config: %s", conf)
-    hass.data[DOMAIN] = conf
+    """Set up the Husqvarna Automower component for Authorization Code Grant."""
+
+    if DOMAIN not in config:
+        return True
+
+    if CONF_CLIENT_ID in config[DOMAIN]:
+        config_flow.HusqvarnaConfigFlowHandler.async_register_implementation(
+            hass,
+            config_entry_oauth2_flow.LocalOAuth2Implementation(
+                hass,
+                DOMAIN,
+                config[DOMAIN][CONF_CLIENT_ID],
+                config[DOMAIN][CONF_CLIENT_SECRET],
+                OAUTH2_AUTHORIZE,
+                OAUTH2_TOKEN,
+            ),
+        )
+        conf = config.get(DOMAIN, {})
+        hass.data[DOMAIN] = conf
 
     return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up this integration using UI."""
+
+    if hass.data.get(DOMAIN) is None:
+        hass.data.setdefault(DOMAIN, {})
+        _LOGGER.info(STARTUP_MESSAGE)
 
     api_key = entry.unique_id
     access_token = entry.data.get(CONF_TOKEN)
@@ -70,7 +92,33 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     except Exception:
         # If we haven't used the refresh_token (ie. been offline) for 10 days,
         # we need to login using username and password in the config flow again.
-        raise ConfigEntryAuthFailed from Exception
+        username = entry.data.get(CONF_USERNAME)
+        password = entry.data.get(CONF_PASSWORD)
+        if not (username and password):
+            raise ConfigEntryAuthFailed from Exception
+        if username and password:
+            get_token = aioautomower.GetAccessToken(
+                api_key,
+                username,
+                password,
+            )
+            access_token = await get_token.async_get_access_token()
+            _LOGGER.debug("access_token: %s", access_token)
+            hass.config_entries.async_update_entry(
+                entry,
+                data={
+                    CONF_TOKEN: access_token,
+                    CONF_USERNAME: username,
+                    CONF_PASSWORD: password,
+                },
+            )
+            try:
+                session = aioautomower.AutomowerSession(api_key, access_token)
+                await session.connect()
+
+                return True
+            except Exception:
+                raise ConfigEntryAuthFailed from Exception
 
     if "amc:api" not in access_token["scope"]:
         raise ConfigEntryAuthFailed(
