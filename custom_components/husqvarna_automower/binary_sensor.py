32ed667d71bd5015773a24a8341d50ca9da4bca8
PrairieSnpr <midnryanmiller@gmail.com>
2022-06-12 13:58:04 -0400

    Add Binary Error Sensor

--- custom_components/husqvarna_automower/binary_sensor.py
+++ custom_components/husqvarna_automower/binary_sensor.py
@@ -10,7 +10,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN
+from .const import DOMAIN, ERRORCODES
 from .entity import AutomowerEntity
 
 _LOGGER = logging.getLogger(__name__)
@@ -27,6 +27,10 @@ async def async_setup_entry(
     )
     async_add_entities(
         AutomowerLeavingDockBinarySensor(session, idx)
+        for idx, ent in enumerate(session.data["data"])
+    )
+    async_add_entities(
+        AutomowerErrorBinarySensor(session, idx)
         for idx, ent in enumerate(session.data["data"])
     )
 
@@ -72,3 +76,39 @@ class AutomowerLeavingDockBinarySensor(BinarySensorEntity, AutomowerEntity):
             return True
         if mower_attributes["mower"]["activity"] != "LEAVING":
             return False
+
+
+class AutomowerErrorBinarySensor(BinarySensorEntity, AutomowerEntity):
+    """Defining the AutomowerErrorSensor Entity."""
+
+    _attr_entity_category = EntityCategory.DIAGNOSTIC
+    _attr_entity_registry_enabled_default = False
+
+    def __init__(self, session, idx):
+        super().__init__(session, idx)
+        self._attr_name = f"{self.mower_name} Error"
+        self._attr_unique_id = f"{self.mower_id}_error"
+
+    @property
+    def is_on(self) -> bool:
+        """Return if the mower is in an error status."""
+        mower_attributes = AutomowerEntity.get_mower_attributes(self)
+        if mower_attributes["mower"]["state"] in [
+            "ERROR",
+            "FATAL_ERROR",
+            "ERROR_AT_POWER_UP",
+        ]:
+            return True
+        return False
+
+    @property
+    def extra_state_attributes(self) -> dict:
+        """Return the specific state attributes of this mower."""
+        mower_attributes = AutomowerEntity.get_mower_attributes(self)
+        if self.is_on:
+            return {
+                "ERROR_CODE": int(mower_attributes["mower"]["errorCode"]),
+                "DESCRIPTION": ERRORCODES.get(mower_attributes["mower"]["errorCode"]),
+            }
+
+        return {"ERROR_CODE": -1, "DESCRIPTION": "No Error"}
