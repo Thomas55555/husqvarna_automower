53d5186acdbf2edf7627402d31237acc9d707ea7
Thomas5555 <thomas.protzner@gmail.com>
2022-07-11 18:45:23 +0000

    remove

--- custom_components/husqvarna_automower/application_credentials.py
+++ custom_components/husqvarna_automower/application_credentials.py
@@ -3,7 +3,7 @@
 from homeassistant.components.application_credentials import AuthorizationServer
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.network import get_url
-from .const import OAUTH2_AUTHORIZE, OAUTH2_TOKEN
+from .const import OAUTH2_AUTHORIZE, OAUTH2_TOKEN, HUSQVARNA_URL
 
 
 async def async_get_authorization_server(hass: HomeAssistant) -> AuthorizationServer:
@@ -17,6 +17,6 @@ async def async_get_authorization_server(hass: HomeAssistant) -> AuthorizationSe
 async def async_get_description_placeholders(hass: HomeAssistant) -> dict[str, str]:
     """Return description placeholders for the credentials dialog."""
     return {
-        "oauth_creds_url": "https://developer.husqvarnagroup.cloud/applications/",
+        "oauth_creds_url": HUSQVARNA_URL,
         "redirect_uri": get_url(hass),
     }
