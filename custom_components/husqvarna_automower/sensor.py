88b1c8e776bc372793f15f3c4a75e2cca50146de
Robin Östlund <me@robinostlund.name>
2023-05-29 08:18:25 +0200

    fix: SyntaxWarning: is with a literal. Did you mean ==?

--- custom_components/husqvarna_automower/sensor.py
+++ custom_components/husqvarna_automower/sensor.py
@@ -203,8 +203,8 @@ SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
         native_unit_of_measurement=PERCENTAGE,
         value_fn=lambda data: data["battery"]["batteryPercent"],
         available_fn=lambda data: False
-        if (data["battery"]["batteryPercent"] is 0)
-        and (data["metadata"]["connected"] is False)
+        if (data["battery"]["batteryPercent"] == 0)
+        and (data["metadata"]["connected"] == False)
         else True,
     ),
     AutomowerSensorEntityDescription(
