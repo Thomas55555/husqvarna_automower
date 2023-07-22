fc552503f47c272f1ea43f3588a4d986e53d5740
Thomas55555 <59625598+Thomas55555@users.noreply.github.com>
2023-07-22 10:29:51 +0000

    black

--- custom_components/husqvarna_automower/tests/test_binary_sensor.py
+++ custom_components/husqvarna_automower/tests/test_binary_sensor.py
@@ -49,9 +49,7 @@ async def test_leaving_dock_sensor(hass: HomeAssistant):
     assert leaving_sensor._attr_unique_id == f"{MWR_ONE_ID}_leaving_dock"
 
     assert leaving_sensor.is_on is False
-    coordinator.data["data"][MWR_ONE_IDX]["attributes"]["mower"][
-        "activity"
-    ] = "LEAVING"
+    coordinator.data["data"][MWR_ONE_IDX]["attributes"]["mower"]["activity"] = "LEAVING"
 
     assert leaving_sensor.is_on is True
 
@@ -74,9 +72,7 @@ async def test_error_sensor(hass: HomeAssistant):
         "description": "No Error",
     }
 
-    coordinator.data["data"][MWR_ONE_IDX]["attributes"]["mower"][
-        "errorCode"
-    ] = 0
+    coordinator.data["data"][MWR_ONE_IDX]["attributes"]["mower"]["errorCode"] = 0
 
     assert error_sensor.extra_state_attributes == {
         "error_code": -1,
@@ -84,9 +80,7 @@ async def test_error_sensor(hass: HomeAssistant):
     }
 
     for e_state in ERROR_STATES:
-        coordinator.data["data"][MWR_ONE_IDX]["attributes"]["mower"][
-            "state"
-        ] = e_state
+        coordinator.data["data"][MWR_ONE_IDX]["attributes"]["mower"]["state"] = e_state
 
         assert error_sensor.is_on is True
         assert error_sensor.extra_state_attributes == {
